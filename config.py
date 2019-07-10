57da919ffec760e95a135a680d8079c88b1f2a1c
Sivamani Vel <28758067+SIVMANIv8@users.noreply.github.com>
2019-07-10 13:42:09 +0530

    Update config.py
    Added  constant

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-CONST_A = 'A'
+class ConfConst:
+    @property
+    def URL(self):
+        return 'https://www.irctc.co.in/'
+    @property
+    def USER(self):
+        return 'sivav8'
+    @property
+    def UNKEY(self):
+        return b'yTOusj8kju9egEFprd3ojT0CDGdKFak38mRuerRmXjo='
+    @property
+    def PASSWORD(self):
+        return b'gAAAAABdJZxAXHfoZpA-emNTn2fln5QtjCeKU7rcRkB8F3QGLiQnCGu4GbPWlBSjEFXMhxWdobDk_mlG1nPSXp3JPhbS8Iy5Ow=='
+    def __getattr__(self,name):
+        raise AttributeError("Undefined Constant")
